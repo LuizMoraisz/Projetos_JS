b3a35a0edf64c1529ef7bbfcf99411811e015316
Luiz Felipe <136415452+LuizMoraisz@users.noreply.github.com>
2025-04-24 08:57:27 -0300

    Calculadora
    Desenvolvendo uma calculadora em JS

---
+++ Projetos/Calculadora/index.js
@@ -0,0 +1,44 @@
+const display = document.getElementById("display");
+const numeros = document.querySelectorAll('[id*=tecla]');
+const operadores = document.querySelectorAll('[class*=operador]');
+
+let novoNumero = true;
+let operador;
+let numeroAnterior;
+
+const operacaoPendente = () => operador !== undefined;  
+
+const calcular = () => {
+    if (operacaoPendente()){
+        const numeroAtual = parseFloat(display.textContent);
+        novoNumero = true;
+        if (operador == "+"){
+            atualizarDisplay(numeroAnterior + numeroAtual);
+        }
+    }
+}
+
+const atualizarDisplay = (texto) => {
+    if (novoNumero){
+        display.textContent = texto;
+        novoNumero = false;
+    }else{
+        display.textContent += texto;
+    }
+}
+
+const InserirNumero =  (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach(numero => numero.addEventListener('click' , InserirNumero));
+
+const selecionarOperador = (evento) => {
+    if (!novoNumero){
+    calcular()
+    
+    novoNumero = true;
+    operador = evento.target.textContent;
+    numeroAnterior =  parseFloat(display.textContent);
+    console.log(operador)
+    }
+}
+
+operadores.forEach(operador => operador.addEventListener('click' , selecionarOperador));
